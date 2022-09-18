aa6fa41b688c58568477f4e56c272fb8adfe4e91
MythosCulture <72172428+MythosCulture@users.noreply.github.com>
2022-09-18 12:11:19 -0500

    Update messageArchive.js

--- messageArchive.js
+++ messageArchive.js
@@ -1,21 +1,21 @@
 const default_CharInfo = {Name: "No Name Given", Pronouns: {pr1: "he/she/they", pr2:"him/her/them", pr3:"his/hers/theirs" } };
-const default_Stats = {Health:100, Sanity:100, Inventory:[], Actions:10};
-const default_log = [
-    {
-        messageId: "P0_000",
-        message: "Opening message. Put game information/warnings here.",
-        choice1: { buttonText:"Continue", nextMsgId:"P0_001"}
-    }
-];
+const default_Stats = {Health:100, Sanity:100, Actions:10};
+const default_Inv = [1];
+const default_log = [ "P0_000"];
 
 //preload defaults into local storage as values that can be retrieved later
 setLocal(default_log, "logDefault");
 setLocal(default_Stats, "statDefault");
 setLocal(default_CharInfo, "charDefault");
+setLocal(default_Inv, "invDefault");
 //set these to default values to start
 setLocal(default_log, "logData");
 setLocal(default_Stats, "statData");
 setLocal(default_CharInfo, "charData");
+setLocal(default_Inv, "invData");
+
+export {Items};
+export {Library};
 
 export function setLocal (setObj, setName) {
     window.localStorage.setItem(setName, JSON.stringify(setObj));
@@ -25,50 +25,82 @@ export function getLocal (getName) {
     return JSON.parse(window.localStorage.getItem(getName));
 }
 
-export {Library};
+function getInvItem(_itemId) { //returns item or undefined
+    let localItem = getLocal("invData");
+    return localItem.find(item => item.id === _itemId);
+}
+function hasItem(_itemId) { //True/False
+    if (getInvItem(_itemId) == undefined) return false;
+    return true;
+}
+function getItem(_itemId) { //same as getObj() in main.js
+    let objIndex = Items.findIndex( function(element) {
+        return element.id === _itemId;
+    });
+    return Items[objIndex];
+}
+
+const Items = [
+    {
+        id: 0,
+        name: "Item 1",
+        description: "Enter description here"
+    },
+    {
+        id: 1,
+        name: "Item 2",
+        description: "This is a description for item 2!"
+    }
+];
 const Library = [
     {
-        messageId: "P0_000",
+        id: "P0_000",
         message: "Opening message. Put game information/warnings here.",
         choice1: { buttonText:"Continue", nextMsgId:"P0_001"}
     },
     {
-        messageId: "P0_001",
+        id: "P0_001",
         message: "This is a test message." +
         "<br> Pick a favorite color.",
-        choice1: { buttonText:"Red", nextMsgId:"P1_000"},
-        choice2: { buttonText:"Blue", nextMsgId:"P2_000"},
-        choice3: { buttonText:"Green", nextMsgId:"P3_000"},
+        choice1: { buttonText:"Test conidtionals for stats", nextMsgId:"P1_000"},
+        choice2: { buttonText:"Test inventory", nextMsgId:"P2_000"},
+        choice3: { buttonText:"option3", nextMsgId:"P3_000"},
     },
     {
-        messageId: "P1_000",
+        id: "P1_000",
         message: "Test Route 1",
         choice1: { buttonText:"Go back", nextMsgId:"P0_001"},
         choice2: getLocal("statData").Health==100?{ buttonText:"Health at 100", nextMsgId:"P0_000"}:undefined
-        //choice2: stats.Health==100?{ buttonText:"Health at 100", nextMsgId:"P0_000"}:undefined
+        //TODO: change getLocal for this action
+        //Game doesnt save to cache after every choice, could potentially be pulling old data
     },
     {
-        messageId: "P2_000",
-        message: "Test Route 2",
+        id: "P2_000",
+        message: hasItem(1)?"You do not see an item.":`You see ${getItem(1).name}`,
         choice1: { buttonText:"Go back", nextMsgId:"P0_001"},
+        choice2: hasItem(1)?undefined:{ buttonText:"Grab item!", nextMsgId: "P2_001"},
+        choice3: hasItem(1)?{ buttonText:"Put item back!", nextMsgId: "P2_002"}:undefined
     },
     {
-        messageId: "P3_000",
+        id: "P2_001",
+        modifyStat: [["Health", -5], ["Inv", 1]],
+        message: `Ah! The ${getItem(1).name} you grabbed was cursed! You lose 5 health.`,
+        choice1: { buttonText:"Continue", nextMsgId:"P2_000"},
+    },
+    {
+        id: "P2_002",
+        modifyStat: [["Health", 5], ["InvRemove", 1]],
+        message: `You respectfully put ${getItem(1).name} back. You gain 5 health.`,
+        choice1: { buttonText:"Continue", nextMsgId:"P2_000"},
+    },
+    {
+        id: "P3_000",
         message: "Test Route 3",
         choice1: { buttonText:"Go back", nextMsgId:"P0_001"},
     },
 ];
-const logArchive = [
-    {
-        messageId: "P0_000",
-        message: "Opening message. Put game information/warnings here.",
-        choice1: { buttonText:"Continue", nextMsgId:"P0_001"}
-    }
-];
 
-const keyEvents = [ //checks if certain choices have been made or events have been triggered
-    {
-        exampleEvent: false,
-        exampleEvent2: true
-    }
-];
+const keyEvents = { //checks if certain choices have been made or events have been triggered
+    exampleEvent: false,
+    exampleEvent2: true
+}
