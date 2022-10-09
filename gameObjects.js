5b992d820669ad6ecac74fee90cb6407c93ca9ab
MythosCulture <72172428+MythosCulture@users.noreply.github.com>
2022-10-09 15:41:38 -0500

    Update gameObjects.js

--- gameObjects.js
+++ gameObjects.js
@@ -1,6 +1,5 @@
 export { choices };
 export { Library };
-
 export { playerObject };
 
 export function setLocal(setObj, setName) {
@@ -11,10 +10,20 @@ export function getLocal(getName) {
     return JSON.parse(window.localStorage.getItem(getName));
 }
 
+const removeInvItem = (player, itemId) => {
+    const itemToRemove = player.inventory.find(item => item.id === itemId);
+    const index = player.inventory.indexOf(itemToRemove);
+    if(index >= 0) player.inventory.splice(index, 1)
+}
+
+export const hasInvItem = (player, itemId) => {
+    if(player.inventory.find(item => item.id === itemId)) return true;
+    return false;
+}
+
 const getItem = itemId => {
     return Library.allItems.find(item => item.id === itemId);
-};
-
+}
 
 const choices = ["choice1", "choice2", "choice3", "choice4"];
 
@@ -26,9 +35,9 @@ let playerObject = {
     actions: 10,
     inventory:[
         {
-            id: 1,
-            name: "Item 2",
-            description: "This is a description for item 2!"
+            id: "Inv_1",
+            name: "Item 1",
+            description: "Enter description here"
         }
     ],
     log: [
@@ -39,18 +48,20 @@ let playerObject = {
     ]
 }
 
+setLocal(playerObject, "playerSave");
+
 const Library = {
-    getPrompt (promptId) {
+    getPrompt(promptId) {
         return this.allPrompts.find(msg => msg.id === promptId);
     },
     allItems: [
         {
-            id: 1,
+            id: "Inv_1",
             name: "Item 1",
             description: "Enter description here"
         },
         {
-            id: 2,
+            id: "Inv_2",
             name: "Item 2",
             description: "This is a description for item 2!"
         }
@@ -65,7 +76,7 @@ const Library = {
             id: "P0_001",
             message: "This is a test message." +
                 "<br> Pick an option below.",
-            choice1: { buttonText: "Test conidtionals for stats", nextId: "P1_000" },
+            choice1: { buttonText: "Test coniditionals for stats", nextId: "P1_000" },
             choice2: { buttonText: "Test inventory", nextId: "P2_000" },
             choice3: { buttonText: "option3", nextId: "P3_000" },
         },
@@ -82,15 +93,43 @@ const Library = {
         {
             id: "P2_000",
             message(player) {
-                if(player.inventory.includes(1)) {
+                console.log(`player has item: ${hasInvItem(player, "Inv_1")}`);
+                console.log(player.inventory);
+
+                if(hasInvItem(player, "Inv_1")) {
                     return "You do not see an item.";
                 } else {
-                    return `You see ${getItem(1).name}.`
+                    return `You see ${getItem("Inv_1").name}.`
                 }
             },
             choice1: { buttonText: "Go back", nextId: "P0_001" },
-            //choice2: inventory.hasItem(0) ? undefined : { buttonText: "Grab item!", nextId: "P2_001" },
-            //choice3: inventory.hasItem(0) ? { buttonText: "Put item back!", nextId: "P2_002" } : undefined
+            choice2(player) {
+                if(hasInvItem(player, "Inv_1")) {
+                    return { buttonText: "Put item back!", nextId: "P2_002" }
+                } else return { buttonText: "Grab item!", nextId: "P2_001" }
+            }
+        },
+        {
+            id: "P2_001",
+            modifyStats(player) {
+                player.inventory.push(getItem("Inv_1"));
+                return player;
+            },
+            message(player) {
+                return `You grab ${getItem("Inv_1").name} and add it to your inventory.`
+            },
+            choice1: {buttonText: "continue", nextId: "P2_000"}
+        },
+        {
+            id: "P2_002",
+            modifyStats(player){
+                removeInvItem(player, "Inv_1");
+                return player;
+            },
+            message(player) {
+             return `You put ${getItem("Inv_1").name} back.`
+            },
+            choice1: {buttonText: "continue", nextId: "P2_000"}
         },
         {
             id: "P3_000",
@@ -99,5 +138,3 @@ const Library = {
         },
     ]
 }
-
-setLocal(playerObject, "playerSave");
